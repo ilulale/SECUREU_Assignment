3fa4d2e7bfcc6ba07985e58fbdb095986fc5537f
ilulale <xiece181959@students.xavierengg.com>
2022-10-26 11:37:07 +0530

    subdomain generation and validation

--- s3-scrape-cli/s3-scrape.py
+++ s3-scrape-cli/s3-scrape.py
@@ -1,11 +1,14 @@
 import os
 import sys
 import json
+from urllib import response
 import xmltodict
 import requests
 import validators
 from multiprocessing.pool import ThreadPool
 import csv
+import re
+from urllib.parse import unquote
 
 
 def init_csv():
@@ -26,7 +29,7 @@ def validate_and_fix_url(url):
     if url_valid:
         return url
     else:
-        url = "https://" + url
+        url = "http://" + url
         url_valid = validators.url(url)
         if url_valid:
             return url
@@ -61,44 +64,73 @@ def get_s3_status(url):
         # write_to_csv(["ERROR", url])
         # return ["ERROR", url]
 
+    # regs3 = r"[\w\-\.]+\.s3\.?(?:[\w\-\.]+)?\.amazonaws\.com|(?<!\.)s3\.?(?:[\w\-\.]+)?\.amazonaws\.com\\?\/[\w\-\.]+"
+    # html = unquote(str(res.content))
+    # print(html)
+    # s3 = re.findall(regs3, html)
 
-def get_permutations(url, limit):
-    url = url.replace(".com", "")
-    perm_data = open("./permutations.json")
-    perm_data = json.load(perm_data)
-    perm_data = perm_data["permutations"][0:limit]
-    perm_arr = []
-    for val in perm_data:
-        tmp_val = val.replace("%s", url, 1)
-        tmp_val = tmp_val.replace("%s", "s3.amazonaws.com")
-        perm_arr.append(tmp_val)
-    for val in perm_data:
-        tmp_val = val.replace("%s", "s3.amazonaws.com.", 1)
-        tmp_val = tmp_val.replace("%s", url)
-        tmp_val = tmp_val.replace("-", "")
-        tmp_val = tmp_val.replace(f".{url}", f"-{url}")
-        tmp_val = tmp_val.replace("com.", "com/")
-        perm_arr.append(tmp_val)
-    return perm_arr
+
+def hosted_on_s3_check(url):
+    # url = validate_and_fix_url(url)
+    try:
+        res = requests.get(url)
+        if "Server" in res.headers:
+            if res.headers["Server"] == "AmazonS3":
+                write_to_csv(["Exists", url])
+                print(
+                    f"Files hosted on s3 with header Server : {res.headers['Server']} URL : {url}"
+                )
+                return url
+            else:
+                return None
+        else:
+            return None
+    except:
+        print(f"[ERROR] {url}")
+
+
+def generate_subdomains(domain, limit):
+    f = open("./subdomains.json")
+    data = json.load(f)
+    data = data[0:limit]
+    sub_list = []
+    for sub in data:
+        sub_list.append(f"{sub}.{domain}")
+    return sub_list
+
+
+def check_site_exits(url):
+    url = validate_and_fix_url(url)
+    try:
+        res = requests.get(url)
+        if res.status_code == 200:
+            return url
+        else:
+            return None
+    except:
+        print(f"Error in {url}")
 
 
 def main():
     domain = input("[+] Enter a URL :\n> ")
-    limit = int(
-        input("Enter number of permutations (value will be multiplied by 2) :\n> ")
-    )
-    domain_permutations = get_permutations(domain, limit)
+
+    subdomains = generate_subdomains(domain, 1000)
+
+    valid_subdomains = []
     pool = ThreadPool()
-    print("Fetching for S3 Buckets....\n")
-    init_csv()
-    result = pool.map(get_s3_status, domain_permutations)
+    print("Validating subdomains....\n")
+    valid_subdomains = pool.map(check_site_exits, subdomains)
+    valid_subdomains = list(filter(lambda item: item is not None, valid_subdomains))
+    print(valid_subdomains)
 
-    result = list(filter(lambda item: item is not None, result))
-    print(f"Total ({len(result)}) buckets found.")
-    print(f"Output is written to > {os.getcwd()}/output.csv")
-
-    # for url in domain_permutations:
-    #     get_s3_status(url)
+    # pool = ThreadPool()
+    # print("Fetching for S3 Buckets....\n")
+    # init_csv()
+    # result = pool.map(hosted_on_s3_check, domains)
+    # result = list(filter(lambda item: item is not None, result))
+    # print(result)
+    # print(f"Total ({len(result)}) buckets found.")
+    # print(f"Output is written to > {os.getcwd()}/output.csv")
 
 
 if __name__ == "__main__":
